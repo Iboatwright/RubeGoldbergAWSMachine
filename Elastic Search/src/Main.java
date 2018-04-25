cfe629f832a1013344443da588f3137c55c3cab6
ChewieSolo97 <mjdonaghy8264@eagle.fgcu.edu>
2018-04-25 16:24:33 -0400

    Add files via upload
    cleaned up packagescrubber

--- Elastic Search/src/Main.java
+++ Elastic Search/src/Main.java
@@ -1,12 +1,9 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URLDecoder;
-import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -17,10 +14,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.amazonaws.util.IOUtils;
 
 public class Main {
 
@@ -40,39 +34,17 @@ public class Main {
 	    String key = "Test Java Files/test1.java".replace('+', ' ');
 	    key = URLDecoder.decode(key, "UTF-8");
 
-	    ListObjectsV2Result result = s3.listObjectsV2("unzipped");
-	    List<S3ObjectSummary> objects = result.getObjectSummaries();
-	    for (S3ObjectSummary os: objects) {
-	        System.out.println("* " + os.getKey());
-	    }
-
-	    
 	    folder = s3.getObject(new GetObjectRequest("unzipped", key));
 	    InputStream test = folder.getObjectContent();
-	    File search = new File("newTest1.java");
-	    OutputStream outputStream = new FileOutputStream(search);
-	    IOUtils.copy(test, outputStream);
-	    outputStream.close();
-	    System.out.println(search.getPath());
+
+	    Scanner s = new Scanner(test).useDelimiter("\\A");
+	    String output = s.hasNext() ? s.next() : "";
+	    s.close();
+	    test.close();
 	    folder.close();
 	    
-		Scanner scan = null;
-		try {
-			scan = new Scanner(search);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		String out = "";
-		while (scan.hasNext()) {
-			out += scan.nextLine();
-			out += "\n";
-		}
-		out = out.replaceAll("package", "// package ");
+		output = output.replaceAll("package", "// package ");
 		
-		s3.putObject("packagescrubber", "scrubbedtest1.java", out);
-
-				scan.close();
-
+		s3.putObject("packagescrubber", "scrubbedtest1.java", output);
 	}
-
 }
